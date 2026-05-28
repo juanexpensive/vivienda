dc563a7a6bbc278c6535d217711253f1a0142e84
juanexpensive <carovaquerojuan@gmail.com>
2026-05-28 10:55:14 +0200

    feat: loading hardcoded dataframe

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from src.ui.layout import render_header
+from src.ui.layout import render_header, render_body
 
 st.set_page_config(page_title="Vivienda", layout="wide")
 render_header()
+render_body()
--- src/ui/layout.py
+++ src/ui/layout.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from src.data.loader import  load_housing_data
 
 def render_header():
     st.title("Vivienda")
     st.subheader("Evolución del precio de la vivienda por provincia en España")
     st.write("Primera versión en construcción")
+
+def render_body(): 
+    housing_data = load_housing_data()
+    st.dataframe(housing_data)
