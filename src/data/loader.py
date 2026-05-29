bb7b2da1de65ebb4f564b5c4cb89c6d195a59a02
juanexpensive <carovaquerojuan@gmail.com>
2026-05-29 11:27:54 +0200

    feat: formatting & filter by period

--- src/data/loader.py
+++ src/data/loader.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 data_path = Path("data") / "housing_sample.csv"
 
-def load_housing_data(): 
+def load_housing_data() -> pd.DataFrame:
     housing_data = pd.read_csv(data_path)
     return housing_data
 
