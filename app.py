8f4a75cd8286f258024b735c3303ea6e645d9bd3
juanexpensive <carovaquerojuan@gmail.com>
2026-05-29 08:01:42 +0200

    feat: basic chart

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from src.data.loader import load_housing_data
 housing_data = load_housing_data()
 column = housing_data.columns.tolist()
 
-st.set_page_config(page_title="Vivienda", layout="wide")
+st.set_page_config(page_title="Spain Insigths", layout="wide")
 render_header()
 render_body(housing_data)
--- src/ui/layout.py
+++ src/ui/layout.py
@@ -1,12 +1,15 @@
 import streamlit as st
+import pandas as pd
 def render_header():
-    st.title("Vivienda")
-    st.subheader("Evolución del precio de la vivienda por provincia en España")
-    st.write("Primera versión en construcción")
+    st.title("Spain Insights")
+    st.subheader("Evolution of housing prices by province in Spain")
+    st.write("First version under construction")
 
 def render_body(housing_data):
     provinces = housing_data["province"].unique()
     selected_province = st.selectbox("selecciona una provincia",options=provinces)
     filtered_data = housing_data[housing_data["province"] == selected_province]
-    st.dataframe(filtered_data)
-    
+    chart_data = filtered_data[["period","value"]]
+    indexed_chart_data = chart_data.set_index("period")
+    st.line_chart(indexed_chart_data)
+    st.dataframe(chart_data)
